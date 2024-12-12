8ad53a4834769150261d29c838ac93d2236749aa
Divyanshu-NK <divyanshu.nk@gmail.com>
2024-12-12 22:39:48 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,39 @@
 import sys
+from src.logger import logging
 
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error: Exception, error_detail: sys) -> str:
+    """
+    Constructs a detailed error message with file name, line number, and error message.
+
+    Args:
+        error (Exception): The exception object.
+        error_detail (sys): System module for accessing traceback information.
+
+    Returns:
+        str: Detailed error message.
+    """
     _, _, exc_tb = error_detail.exc_info()  # exc_tb gives info about error location and type
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}].".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-    
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+        
+    error_message = f"Error occurred in python script name [{file_name}] line number [{line_number}] error message [{str(error)}]."
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)  # Corrected this line
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-        
-    def __str__(self):
+    def __init__(self, error_message: Exception, error_detail: sys):
+        """
+        Custom exception class that provides detailed error information.
+
+        Args:
+            error_message (Exception): The original exception message.
+            error_detail (sys): System module for accessing traceback information.
+        """
+        super().__init__(str(error_message))  # Ensures base Exception initialization
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self) -> str:
         return self.error_message
